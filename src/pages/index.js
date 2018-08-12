73aa56093e2e16c7f284c6f7101b998cbf464752
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-08-13 04:07:30 +0530

    Added title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +9,9 @@ class Index extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>GSoC'18 @ Jitsi | Akshit Kr Nagpal</title>
+                </Helmet>
                 <Introduction />
                 <Phase1 />
                 <Phase2 />
