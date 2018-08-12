4d293be7217d6c57130977bf16760b4e52f22c31
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-08-13 01:23:43 +0530

    Updates

---
+++ 2-ec7ebd082b42d7f57fbd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{208:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=2-ec7ebd082b42d7f57fbd.js.map
